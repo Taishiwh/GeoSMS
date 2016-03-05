4ba300bec8312d4b39060304d0e3a7a0b7cd98e6
Taishi Hasegawa <taishi.14h@gmail.com>
2016-03-05 01:48:58 +0000

    first commit

---
+++ GeoSMS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GeoSMS
+//
+//  Created by Taishi Worawongwasu Hasegawa on 3/5/16.
+//  Copyright © 2016 Taishi Worawongwasu Hasegawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
